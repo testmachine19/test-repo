b2922aaba961a5775129cbf23e6feed6e3a084ee
testmachine19 <patysson@mail.ru>
2023-10-22 01:41:39 +0300

    Домашнее задание 2.1.3

---
+++ task_score.js
@@ -0,0 +1,14 @@
+let taskScoreIntroduction = 100;
+let taskScoreGit = 319;
+let taskScoreJS = 260;
+
+let total = taskScoreIntroduction + taskScoreGit + taskScoreJS;
+
+console.log(total);
+console.log(total / 4);
+
+let myName = "Slava";
+let mySurname = "Vorobyov";
+
+console.log(`${mySurname} ${myName} gets ${total} for all tasks`);
+console.log(`${mySurname} ${myName} gets ${total / 4} average for all tasks`);
