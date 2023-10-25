b0ae059f8c557ee29aa13aa8b51613877730d5db
testmachine19 <patysson@mail.ru>
2023-10-25 12:02:39 +0300

    Домашнее задание 2.1.4-correct

--- task_score.js
+++ task_score.js
@@ -7,3 +7,15 @@ let averageScore = total / 3;
 
 console.log(total);
 console.log(averageScore);
+
+const myName = "Slava";
+const mySurname = "Vorobyov";
+
+//console.log(`${mySurname} ${myName} gets ${total} for all tasks`);
+//console.log(`${mySurname} ${myName} gets ${total / 4} average for all tasks`);
+
+console.log(myName.length);
+console.log(`${myName.slice(0, 1)}. ${mySurname} gets ${total} for all tasks`);
+console.log(
+  `${myName.slice(0, 1)}. ${mySurname} gets ${averageScore} for average tasks`
+);
